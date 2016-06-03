698e85a40354035ac1243be911fa524e102d3984
Guilherme Diego <guidiego.expgames@gmail.com>
2016-06-03 11:41:00 +0000

    Added main file

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Component from './component.js';
+import Store from './store.js';
+import Action from './action.js';
+import Middleware from './middleware.js';
+import Dispatcher from './utils/dispatch';
+
+let Dispatch = Dispatcher.dispatch;
+
+export Component;
+export Store;
+export Action;
+export Dispatcher;
+export Dispatch;
+export Middleware;
+
+export default {Component, Store, Action, Dispatcher, Dispatch, Middleware};
