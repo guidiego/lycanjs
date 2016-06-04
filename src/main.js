1ac14a20d1ac0698eb892d18229f3238b2aed077
Guilherme Diego <guidiego.expgames@gmail.com>
2016-06-04 16:47:58 +0000

    Remove dispatch from main and create a standalone function

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-import Dispatcher from './utils/dispatch';
-
-let Dispatch = (type, data) => Dispatcher.dispatch({type, ...data});
-
+export { default as Dispatch} from './utils/dispatch'
+export { default as Dispatcher } from './utils/dispatcher'
 export { default as Component } from './component.js';
 export { default as Store } from './store.js';
 export { default as Action } from './action.js';
 export { default as Middleware } from './middleware.js';
 export { default as Request } from './utils/request';
-
-export default { Dispatcher, Dispatch }
